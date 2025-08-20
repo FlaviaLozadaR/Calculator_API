b65cb4c0aa21a0e2636ba693d72ce774b13c3834
FlaviaLozadaR <a2024112463@estudiantes.upsa.edu.bo>
2025-08-20 19:54:22 -0400

    Ya cree el endpoint de get/sum

--- Controllers/CalculatorController.cs
+++ Controllers/CalculatorController.cs
@@ -7,4 +7,14 @@ public class CalculatorController
     [Route(routeTemplate: "api/[controller]")]
     [ApiController]
     public class calculatorController : ControllerBase
+    {
+        {
+            [HttpGet("sum")]
+            public IActionResult Sum([FromQuery] int a, [FromQuery] int b)
+            {
+                int resultado = a + b;
+                return Ok(new { resultado });
+            }
+        }
+    }
 }
